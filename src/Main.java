cbadc1a5f26a0cf4bed12d582f80c6dc49b15619
OliveSerg <oliveserg@gmail.com>
2021-09-22 14:30:21 -0400

    Change connection code to get information from .env file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
 
 public class Main {
 
-
-    public static ArrayList<String> get() throws Exception{
+    public static ArrayList<String> get() throws Exception {
         String table = "authors";
 
         try {
@@ -12,11 +18,13 @@ public class Main {
             PreparedStatement statement = con.prepareStatement("SELECT AuthorID, Aname FROM " + table);
             ResultSet result = statement.executeQuery();
             ArrayList<String> array = new ArrayList<String>();
-            while(result.next()){
+            while (result.next()) {
                 array.add(result.getString("Aname").trim());
             }
             return array;
-        } catch(Exception e) { System.out.println(e); }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         return null;
     }
 
@@ -25,29 +33,40 @@ public class Main {
 
         try {
             Connection con = getConnection();
-            PreparedStatement statement = con.prepareStatement("SELECT Institution, email, phone, citations, hindex, i10index, influencescore FROM " + table + " WHERE Aname = \"" + name + "\";");
+            PreparedStatement statement = con.prepareStatement(
+                    "SELECT Institution, email, phone, citations, hindex, i10index, influencescore FROM " + table
+                            + " WHERE Aname = \"" + name + "\";");
             ResultSet result = statement.executeQuery();
             return new Author(result);
 
-        } catch(Exception e) { System.out.println(e); }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         return null;
     }
 
-    public static Connection getConnection() throws Exception{
-        try{
-            String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:3306/alidatabase";
-            String username = "root";
-            String password = "M5198189565m-";
-            Class.forName(driver);
-            Connection conn = DriverManager.getConnection(url, username, password);
+    public static Connection getConnection() throws Exception {
+        Scanner scanner = new Scanner(new File("../.env"));
+        HashMap<String, String> config = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split("=");
+            config.put(line[0].trim(), line[1].trim());
+        }
+        if (config.get("DB_DRIVER").equals("mysql")) {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+
+        String connectionUrl = String.format("jdbc:%s://%s:3306/%s", config.get("DB_DRIVER"), config.get("DB_HOST"),
+                config.get("DB_NAME"));
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, config.get("DB_USER"),
+                config.get("DB_PASSWORD"));) {
             System.out.println("Connected");
             return conn;
-        } catch(Exception e) { System.out.println(e); }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
-
-
-
 }
--- src/gui.java
+++ src/gui.java
@@ -5,9 +5,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import keeptoo.KButton;
+import com.k33ptoo.components.KButton;
 import java.awt.MouseInfo;
-
 
 public class gui extends Main implements MouseListener, ActionListener {
     private static JFrame frame;
@@ -23,12 +22,13 @@ public class gui extends Main implements MouseListener, ActionListener {
     private static String nameClicked;
 
     public gui() throws Exception {
-        frame= new JFrame("BioWindow"); //create a frame
+        frame = new JFrame("BioWindow"); // create a frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
-        makePanel(); //create the panel
+        makePanel(); // create the panel
         frame.setVisible(true);
     }
+
     public void makePanel() throws Exception {
         panel = new JPanel();
         panel.setBackground(new Color(17, 44, 80));
@@ -41,16 +41,16 @@ public class gui extends Main implements MouseListener, ActionListener {
         title.setFont(new Font("Serif", Font.BOLD, 35));
         title.setBounds(350, 15, title.getMaximumSize().width, title.getMaximumSize().height);
         int y = 40;
-        for(int i = 0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             name = new JLabel(authorNames.get(i));
             y = y + 50;
-//            jLabelArr[r].addMouseListener(this);
+            // jLabelArr[r].addMouseListener(this);
             name.setForeground(new Color(255, 199, 0));
             name.setFont(new Font("Serif", Font.BOLD, 25));
             name.setBounds(340, y, name.getMaximumSize().width, name.getMaximumSize().height);
             array.add(name);
         }
-        for(int i = 0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             array.get(i).addMouseListener(this);
             panel.add(array.get(i));
         }
@@ -62,7 +62,7 @@ public class gui extends Main implements MouseListener, ActionListener {
         frame2 = new JFrame(nameClicked);
         frame2.setSize(800, 600);
         Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-        frame2.setLocation((int)mousePosition.getX(),(int)mousePosition.getY()+10);
+        frame2.setLocation((int) mousePosition.getX(), (int) mousePosition.getY() + 10);
         panel2 = new JPanel();
         panel2.setBackground(new Color(17, 44, 80));
         panel2.setBounds(0, 0, WIDTH, HEIGHT);
@@ -73,11 +73,11 @@ public class gui extends Main implements MouseListener, ActionListener {
         title2.setForeground(new Color(255, 199, 0));
         title2.setFont(new Font("Impact", Font.BOLD, 35));
         title2.setBounds(350, 15, title2.getMaximumSize().width, title2.getMaximumSize().height);
-        for(int i = 0; i<7; i++){
+        for (int i = 0; i < 7; i++) {
             JLabel bioInfo = new JLabel(bio.get(i));
             bioInfo.setForeground(new Color(255, 199, 0));
             bioInfo.setFont(new Font("Serif", Font.BOLD, 25));
-            bioInfo.setBounds(260, 70+50*i, bioInfo.getMaximumSize().width, bioInfo.getMaximumSize().height);
+            bioInfo.setBounds(260, 70 + 50 * i, bioInfo.getMaximumSize().width, bioInfo.getMaximumSize().height);
             panel2.add(bioInfo);
         }
         backButton = new KButton();
@@ -106,14 +106,13 @@ public class gui extends Main implements MouseListener, ActionListener {
         new gui();
         getConnection();
 
-
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        for(int i = 0; i<array.size(); i++){
-            if(e.getSource() == array.get(i)) {
-                nameClicked = ((JLabel)e.getSource()).getText();
+        for (int i = 0; i < array.size(); i++) {
+            if (e.getSource() == array.get(i)) {
+                nameClicked = ((JLabel) e.getSource()).getText();
                 try {
                     makePopUpWindow();
                 } catch (Exception exception) {
@@ -137,6 +136,7 @@ public class gui extends Main implements MouseListener, ActionListener {
     public void mouseEntered(MouseEvent e) {
 
     }
+
     @Override
     public void mouseExited(MouseEvent e) {
 
@@ -144,7 +144,7 @@ public class gui extends Main implements MouseListener, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == backButton){
+        if (e.getSource() == backButton) {
             frame2.setVisible(false);
         }
     }
